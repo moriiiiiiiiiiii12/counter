d7cb0244d6ee55e3dc79446bb0b67e1d80009ff4
moriiiiiiiiiiii12 <moriiiiiiiiiiii12@gmail.com>
2025-03-16 14:30:55 +0300

    edit scripts

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,10 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
