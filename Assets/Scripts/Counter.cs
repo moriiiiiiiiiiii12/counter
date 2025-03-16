3f1760ff53c1748c29e83f174d6b5eef238ebe49
moriiiiiiiiiiii12 <moriiiiiiiiiiii12@gmail.com>
2025-03-16 14:28:59 +0300

    Create project

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _countSteps;
+    [SerializeField] private float _timeStep = 0.5f;
+    [SerializeField, Min(0)] private float _count = 0f;
+
+    private Coroutine _countCoroutine;
+
+    public bool IsCounting { get; private set; } = false;
+    public float Count => _count;
+
+    public event UnityAction CountingChanged;
+    public event UnityAction CountChanged;
+
+    public void Switching()
+    {
+        if (_countCoroutine == null) 
+        {
+            Debug.Log("Полетело");
+            IsCounting = true;
+
+            _countCoroutine = StartCoroutine(StartCount());
+
+            CountingChanged?.Invoke();
+        }
+        else 
+        {
+            Debug.Log("Остановка");
+            IsCounting = false;
+
+            StopCoroutine(_countCoroutine);
+            
+            _countCoroutine = null;
+
+            CountingChanged?.Invoke();
+        }
+    }
+
+    private IEnumerator StartCount()
+    {
+        while (IsCounting)
+        {
+            _count += _countSteps;
+            CountChanged?.Invoke();
+            
+            Debug.Log(_count.ToString());
+
+            yield return new WaitForSecondsRealtime(_timeStep);
+        }
+    }
+}
---
+++ Assets/Scripts/CounterView.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private TextMeshProUGUI _textButton;
+    [SerializeField] private Counter _counter;
+
+    private void Start()
+    {
+        _text.text = _counter.Count.ToString();
+
+        DisplayStatusButton();
+
+    }
+
+    private void OnEnable()
+    {
+        _counter.CountChanged += DisplayCount;
+        _counter.CountingChanged += DisplayStatusButton;
+    }
+
+    private void OnDisable()
+    {
+        _counter.CountChanged -= DisplayCount;
+        _counter.CountingChanged -= DisplayStatusButton;
+    }
+
+    private void DisplayCount()
+    {
+        _text.text = _counter.Count.ToString();
+    }
+
+    private void DisplayStatusButton()
+    {
+        if (_counter.IsCounting == false)
+        {
+            _textButton.text = "Включить";
+        }
+        else
+        {
+            _textButton.text = "Отключить";
+        }
+    }
+}
